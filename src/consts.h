1ff8d27f35f3c93d03e5c9b593e6a1e668e8e483
leonreino <58917386+leonreino@users.noreply.github.com>
2021-06-28 15:45:13 +0300

    Updated: some constants

--- src/consts.h
+++ src/consts.h
@@ -1,22 +1,29 @@
 #pragma once
 
-#define HEIGHT      15
-#define WIDTH       15
-#define MAX_WIDTH   100
-#define MAX_HEIGHT  100
+#define DEBUG 0
+
+#define INIT_HEIGHT 10
+#define INIT_WIDTH  10
+#define MAX_HEIGHT  50
+#define MAX_WIDTH   50
+#define MIN_HEIGHT  8
+#define MIN_WIDTH   8
+
+#define INIT_SPEED_CONST 5
+#define MAX_SPEED_CONST  10 
+#define MIN_SPEED_CONST  0.5 
 
 #define INIT_SNAKE_LEN      3
 #define INIT_SNAKE_HEAD_X   2
 #define INIT_SNAKE_HEAD_Y   3
 #define MAX_SNAKE_NAME_LEN  10
-#define MAX_SNAKE_INIT_LEN  5
+#define MAX_SNAKE_INIT_LEN  3
 
 #define MAP_SNAKE_BODY '#'
 #define MAP_SNAKE_HEAD '+'
 #define MAP_FOOD       '8'
 #define MAP_BLANK      '-'
 
-#define SLEEP_TIME 1000*0.2
 
 enum Direction
 {
