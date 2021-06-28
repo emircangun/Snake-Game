f12e425de665d9f0f55a2d10c54dbb9eda4238f5
leonreino <58917386+leonreino@users.noreply.github.com>
2021-06-28 11:17:45 +0300

    Added: main.cpp to run game

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include "./src/Game.h"
+#include "./src/Snake.h"
+#include "./src/Map.h"
+
+#include <iostream>
+#ifdef _WIN32
+    #include <windows.h>
+#endif
+#ifdef __unix__
+    #include <unistd.h>
+#endif
+
+using namespace std;
+
+void GameSleep(int sleepMs)
+{
+    #ifdef _WIN32
+        Sleep(sleepMs);
+    #endif
+    #ifdef __unix__
+        usleep(sleepMs * 1000);
+    #endif
+}
+
+int main()
+{
+    string name;
+    cout << "Name for your snake: ";
+    cin >> name;
+
+    Snake snake;
+    snake.InitSnake(name);
+    Map map;
+    map.CreateMap();
+
+    Game game(map, snake);
+    game.GameStart();
+    while (!game.IsGameEnd())
+    {
+        game.GameLoop();
+        GameSleep(1000 * 2); // 1000ms
+    }
+
+    return 0;
+}
