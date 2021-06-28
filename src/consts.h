e2ea6ac5e1ef6318954dfad44034d369442290e8
leonreino <58917386+leonreino@users.noreply.github.com>
2021-06-28 13:11:12 +0300

    Updated: some constants and SLEEP_TIME added

--- src/consts.h
+++ src/consts.h
@@ -1,26 +1,28 @@
 #pragma once
 
-#define WIDTH 10
-#define HEIGHT 10
-#define MAX_WIDTH 100
-#define MAX_HEIGHT 100
+#define HEIGHT      15
+#define WIDTH       15
+#define MAX_WIDTH   100
+#define MAX_HEIGHT  100
 
-#define INIT_SNAKE_LEN 3
-#define INIT_SNAKE_HEAD_X WIDTH/2
-#define INIT_SNAKE_HEAD_Y HEIGHT/2
-#define MAX_SNAKE_NAME_LEN 10
-#define MAX_SNAKE_INIT_LEN 5
+#define INIT_SNAKE_LEN      3
+#define INIT_SNAKE_HEAD_X   2
+#define INIT_SNAKE_HEAD_Y   3
+#define MAX_SNAKE_NAME_LEN  10
+#define MAX_SNAKE_INIT_LEN  5
 
 #define MAP_SNAKE_BODY '#'
 #define MAP_SNAKE_HEAD '+'
 #define MAP_FOOD       '8'
 #define MAP_BLANK      '-'
 
+#define SLEEP_TIME 1000*0.2
+
 enum Direction
 {
+    NOP, // stay, no operation
     UP,
     RIGHT,
     DOWN,
-    LEFT,
-    NOP // stay, no operation
+    LEFT
 };
