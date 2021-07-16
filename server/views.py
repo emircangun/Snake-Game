7dcb01721df477d7de389a68acd04cec4af2c0b0
leonreino <58917386+leonreino@users.noreply.github.com>
2021-07-16 11:46:40 +0300

    view functions initiliazed

--- server/views.py
+++ server/views.py
@@ -2,5 +2,30 @@
     login
     sign_up
     
-
+    get_game
+    add_game
 '''
+
+#  burada password hash et, api arası hash olmayan password geçişi sağlıklı mıdır?
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# existing user
+def login():
+    '''
+        return succeed or failed, error kodları göster
+    '''
+
+    pass
+
+# new user
+def signup():
+    pass
+
+
+def get_game(username, game_id):
+    pass
+
+
+def add_game(username):
+    pass
