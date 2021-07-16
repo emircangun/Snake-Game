d3c9718da399498b6b95726fab3ba4d5c50c1d78
leonreino <58917386+leonreino@users.noreply.github.com>
2021-07-16 11:46:17 +0300

    Database connection, api urls and login added

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask
-
+import views
+from database import Database
 
 # creating app and getting config from module
 app = Flask(__name__)
 app.config.from_object("configmodule.DevelopmentConfig")
 
+# creating database and adding to config
+db = Database()
+app.config["db"] = db
 
+# /api/users/login
+# /api/users/signup
+app.add_url_rule(
+    "/api/users/login", view_func=views.login, methods=["POST"]
+)
+app.add_url_rule(
+    "/api/users/signup", view_func=views.signup, methods=["POST"]
+)
 
+# /api/<username>/<game_id>  -> get
+# /api/<username>/add_game   -> post
+app.add_url_rule(
+    "/api/users/<string:username>/<int:game_id>", view_func=views.get_game, methods=["GET"]
+)
+app.add_url_rule(
+    "/api/users/<string:username>/add_game", view_func=views.add_game, methods=["POST"]
+)
 
 
 
