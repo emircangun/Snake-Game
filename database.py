7cf7418fb031c0796c51e8cf3eadc9e0ac621ba4
leonreino <58917386+leonreino@users.noreply.github.com>
2021-07-17 13:42:03 +0300

    database module added

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
-from types import resolve_bases
 from pymongo import MongoClient
 from flask import current_app
+from schemas import UserSchema, GameSchema
+from werkzeug.security import check_password_hash
 import json
 
 class Database():
@@ -9,18 +10,37 @@ class Database():
         add_user(user)
     '''
     def __init__(self):
-        # client = MongoClient(current_app.config["DATABASE_URI"])
-        client = MongoClient("mongodb+srv://emircangun:PdLym640NLg0jI7S@snake-database.gd2rk.mongodb.net/snake-database?retryWrites=true&w=majority")
+        # client = MongoClient(current_app.config.get("DATABASE_URI"))
+        client = MongoClient("mongodb://emircangun:leonreino@snake-database-shard-00-00.gd2rk.mongodb.net:27017,snake-database-shard-00-01.gd2rk.mongodb.net:27017,snake-database-shard-00-02.gd2rk.mongodb.net:27017/snake-database?ssl=true&replicaSet=atlas-43tm8p-shard-0&authSource=admin&retryWrites=true&w=majority")
         self.db = client["snake-database"]
         self.user_collection = self.db["users"]
 
+    def get_user(self, username):
+        query = { "username": username }
+        user = self.user_collection.find_one(query)
+        return user
 
+    def add_user(self, username, hashed_password):
+        user = UserSchema(username, hashed_password, games=[])
+        user_json = user.user_to_json()
+        self.user_collection.insert_one(user_json)
 
+    def add_game(self, username, score, direction_history):
+        query = { "username": username }
+        user = self.user_collection.find_one(query)
+        games = user.get("games")
+        game = GameSchema(score, direction_history)
+        game_json = game.game_to_json()
+        games.append(game_json)
+        self.user_collection.update_one({ "username": username}, { "$set": { "games": games } })
 
-
-
-
-
+    def check_user(self, username, password):
+        user = self.get_user(username)
+        if user is not None:
+            if password:
+                if check_password_hash(user.get("password"), password):
+                    return True
+        return False
 
 
 class TestDatabase(Database):
@@ -32,11 +52,22 @@ class TestDatabase(Database):
             response.append(user)
         return json.dumps(response)
 
+    def test_get_user(self):
+        return self.get_user("sdfa")
+
+    def test_add_user(self):
+        self.add_user("test2", "test2")
+
+    def test_add_game_to_user(self):
+        self.add_game("test3", [2, 1, 0, 3])
+
 
 if __name__ == "__main__":
     db = TestDatabase()
-    print(db.test_fetch_data())
-
+    # print(db.test_fetch_data())
+    # print(db.test_get_user())
+    # db.test_add_user()
+    db.test_add_game_to_user()
 
 
 
