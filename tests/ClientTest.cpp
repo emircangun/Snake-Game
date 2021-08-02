ae6fb4967c4a6bb3556385e86a8b069eb208b2a3
leonreino <58917386+leonreino@users.noreply.github.com>
2021-08-02 13:17:34 +0300

    testing maxScore and Login function

--- tests/ClientTest.cpp
+++ tests/ClientTest.cpp
@@ -1,9 +1,16 @@
 #include "../client/Client.h"
+#include <iostream>
 
 int main()
 {
     Client test_user = Client();
-    test_user.Login();
+    try {
+        test_user.Login();
+        test_user.GetMaxScoreFromDB();
+    } catch(const char* err) {
+        std::cout << err << std::endl;
+    }
+    
 
     return 0;
 }
