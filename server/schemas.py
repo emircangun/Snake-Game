5026348afd3a26635071291ffccaac1d9580db69
leonreino <58917386+leonreino@users.noreply.github.com>
2021-08-04 13:49:29 +0300

    maxScore is added to UserSchema

--- server/schemas.py
+++ server/schemas.py
@@ -5,15 +5,17 @@
 
 
 class UserSchema:
-    def __init__(self, username, hashed_password, games):
+    def __init__(self, username, hashed_password, games, max_score):
         self.username = username
         self.password = hashed_password
+        self.max_score = max_score
         self.games = games
 
     def user_to_json(self):
         user_json = {
             "username": self.username,
             "password": self.password,
+            "max_score": self.max_score,
             "games": self.games
         }
 
