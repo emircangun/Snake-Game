7d03f4997f66c6065484720106233332b3b54e48
leonreino <58917386+leonreino@users.noreply.github.com>
2021-08-05 15:05:59 +0300

    Added new couts

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,68 +4,68 @@
 #include <iomanip>
 
 #ifdef _WIN32
-    #include <windows.h>
-    #include <conio.h>
+#include <windows.h>
+#include <conio.h>
 #else
-    #include <unistd.h>
-    #include <termios.h>
-	#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
 #endif
 
 #ifndef _WIN32
-	int kbhit(void)
-	{
-		struct termios oldt, newt;
-		int ch;
-		int oldf;
+int kbhit(void)
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
-		tcgetattr(STDIN_FILENO, &oldt);
-		newt = oldt;
-		newt.c_lflag &= ~(ICANON | ECHO);
-		tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-		oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-		fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-		ch = getchar();
+    ch = getchar();
 
-		tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-		fcntl(STDIN_FILENO, F_SETFL, oldf);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-		if(ch != EOF)
-		{
-		ungetc(ch, stdin);
-		return 1;
-		}
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
 
-		return 0;
-	}
+    return 0;
+}
 
-    char getch()
-	{
-		char c;
-		system("stty raw");
-		c = getchar();
-		system("stty sane");
-		return(c);
-	}
+char getch()
+{
+    char c;
+    system("stty raw");
+    c = getchar();
+    system("stty sane");
+    return (c);
+}
 #endif
 
 void ClearScreen()
 {
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
 
-char UP_KEY     = 'w';
-char DOWN_KEY   = 's';
-char RIGHT_KEY  = 'd';
-char LEFT_KEY   = 'a';
-char STOP_KEY   = 'r';
+char UP_KEY = 'w';
+char DOWN_KEY = 's';
+char RIGHT_KEY = 'd';
+char LEFT_KEY = 'a';
+char STOP_KEY = 'r';
 
-Game::Game(Map& _map, Snake& _snake, Client& _user)
+Game::Game(Map &_map, Snake &_snake, Client &_user)
     : map(_map), snake(_snake), game_over(false), user(_user)
 {
 }
@@ -85,26 +85,26 @@ void Game::StartGame()
 
 char Game::TakeInput()
 {
-    #ifdef _WIN32
+#ifdef _WIN32
     if (_kbhit())
     {
         char ch = _getch();
         return ch;
     }
-    #endif
+#endif
 
-    #ifndef _WIN32
+#ifndef _WIN32
     if (kbhit())
     {
         char ch = getch();
         return ch;
     }
-    #endif
+#endif
 
     return 0;
 }
 
-bool Game::HandleInput(char input, Direction& direction)
+bool Game::HandleInput(char input, Direction &direction)
 {
     if (input == UP_KEY)
     {
@@ -147,7 +147,8 @@ bool Game::StopGame(bool exit)
         // change keys
         // resume
         // quit
-        do {
+        do
+        {
             std::cout << "1. Resume\n";
             std::cout << "2. Change keys\n";
             std::cout << "3. Quit game\n";
@@ -163,17 +164,18 @@ bool Game::StopGame(bool exit)
         {
             // aynı harften yapamamayı ekle
             int change_key;
-            do {
+            do
+            {
                 std::cout << "\nChoose the key you want to change.\n";
-                std::cout << "1. Up key    : " << UP_KEY << "\n"; 
-                std::cout << "2. Down key  : " << DOWN_KEY << "\n"; 
-                std::cout << "3. Right key : " << RIGHT_KEY << "\n"; 
+                std::cout << "1. Up key    : " << UP_KEY << "\n";
+                std::cout << "2. Down key  : " << DOWN_KEY << "\n";
+                std::cout << "3. Right key : " << RIGHT_KEY << "\n";
                 std::cout << "4. Left key  : " << LEFT_KEY << "\n";
                 std::cout << "5. Return menu\n";
                 std::cout << ">>> ";
                 std::cin >> change_key;
-            } while(change_key < 1 || change_key > 5);
-            
+            } while (change_key < 1 || change_key > 5);
+
             if (change_key == 5)
                 return false;
 
@@ -193,7 +195,6 @@ bool Game::StopGame(bool exit)
         {
             return true;
         }
-
     }
     return false;
 }
@@ -202,7 +203,7 @@ bool Game::StopGame(bool exit)
 bool Game::GameLoop()
 {
     ClearScreen();
-    
+
     char input = TakeInput();
     Direction direction;
     // true if input is a direction
@@ -212,7 +213,8 @@ bool Game::GameLoop()
     if (!is_input_stop)
         quit = StopGame(is_input_stop);
 
-    if (quit) return true;
+    if (quit)
+        return true;
 
     // go on play
     direction_history.push_back(direction);
@@ -231,8 +233,8 @@ bool Game::GameLoop()
             int _score = snake.GetScore();
             double tmp = Speed;
             if (_score >= 10 && _score < 40 && _score % 10 == 0) // 10 20 30
-                tmp += 3 + _score / 10; // 1 2 3
-            else if (_score >= 40 && _score % 10 == 0) // 40 50 60 ...
+                tmp += 3 + _score / 10;                          // 1 2 3
+            else if (_score >= 40 && _score % 10 == 0)           // 40 50 60 ...
                 tmp += 8;
 
             if (tmp < MAX_SPEED_CONST)
@@ -242,16 +244,18 @@ bool Game::GameLoop()
         }
     }
 
-    std::cout << std::string(Width - 3, ' ') << "SNAKE GAME" << "\n\n";
+    std::cout << std::string(Width - 3, ' ') << "SNAKE GAME"
+              << "\n\n";
     std::cout << "Up Key    : " << UP_KEY << "\n";
     std::cout << "Down Key  : " << DOWN_KEY << "\n";
     std::cout << "Right Key : " << RIGHT_KEY << "\n";
     std::cout << "Left Key  : " << LEFT_KEY << "\n";
     std::cout << "Pause Key : " << STOP_KEY << "\n\n";
 
-    std::cout << "Name:  " << user.GetUsername() << "\nMaximum Score: " << user.GetMaxScore() << "\n\n";
-    
-    std::cout << "Current Score: " << snake.GetScore() << "\nSpeed: " << Speed << "\n\n";
+    std::cout << "Name: " << user.GetUsername() << "\nMaximum Score: " << user.GetMaxScore() << "\n\n";
+
+    std::cout << "Current Score: " << snake.GetScore() << "\nSpeed: " << Speed << "\n";
+    std::cout << "\n(Press " << STOP_KEY << " to stop game.)\n";
 
     map.UpdateMapAndDraw(snake);
     return false;
@@ -265,7 +269,8 @@ void Game::EndGame()
     std::cout << "--- Verify yourself to keep this score ---\n";
     std::string password;
     bool isPasswordCorrect = true;
-    do {
+    do
+    {
         std::cout << "Password: ";
         std::cin >> password;
         isPasswordCorrect = user.AddGame(password, this->direction_history, snake.GetScore());
