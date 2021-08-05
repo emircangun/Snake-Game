733aee45ea07612b71aba4f1917bf311f4ce12ef
leonreino <58917386+leonreino@users.noreply.github.com>
2021-08-05 15:06:37 +0300

    StartGame is included and change on all main

--- main.cpp
+++ main.cpp
@@ -1,154 +1,13 @@
-#include "./src/Game.h"
-#include "./src/Snake.h"
-#include "./src/Map.h"
-#include "./src/consts.h"
-#include "./client/Client.h"
-
-#include <iostream>
-#include <sstream>
-#include <cstring>
-#include <ctime>
-
-#ifdef _WIN32
-    #include <windows.h>
-#else
-    #include <unistd.h>
-#endif
+#include "./src/StartGame.h"
 
 unsigned int Width;
 unsigned int Height;
 double Speed;
 
-//
-void GameSleep(int sleepMs)
-{
-    #ifdef _WIN32
-        Sleep(sleepMs);
-    #else
-        usleep(sleepMs * 1000);
-    #endif
-}
-
-//
-#define HELP_COUT {               \
-                    std::cout << "\nExample run - (width, height, speeed):  ./main 20 20 1.5\n"; \
-                    std::cout << "Example run - initial values:             ./main\n\n"; \
-                    std::cout << "-W : Width , max " << MAX_WIDTH << ", min " << MIN_WIDTH << ", init " << INIT_WIDTH << "\n"; \
-                    std::cout << "-H : Height, max " << MAX_HEIGHT << ", min " << MIN_HEIGHT << ", init " << INIT_HEIGHT << "\n"; \
-                    std::cout << "-S : Speed , max " << MAX_SPEED_CONST << ", min " << MIN_SPEED_CONST  << ", init " << INIT_SPEED_CONST << "\n\n"; \
-                  }
-
-//
-void CommandLineHandling(int argc, char* argv[])
-{
-    if (argc == 1)
-    {
-        Width  = INIT_WIDTH;
-        Height = INIT_HEIGHT;
-        Speed  = INIT_SPEED_CONST;
-    }
-    else if (argc == 2)
-    {
-        if (strcmp(argv[1], "-help") == 0)
-            throw "Help";
-    }
-    else if (argc == 4)
-    {
-        std::stringstream ss;
-        {
-            int tmp_width;
-            ss << argv[1];  
-            ss >> tmp_width;
-            ss.clear();
-            if (tmp_width < MIN_WIDTH || tmp_width > MAX_WIDTH)
-                throw "Please, select from the appropriate range.";
-            else
-                Width = tmp_width;
-        }
-        {
-            int tmp_height;
-            ss << argv[2];  
-            ss >> tmp_height;
-            ss.clear();
-            if (tmp_height < MIN_HEIGHT || tmp_height > MAX_HEIGHT)
-                throw "Please, select from the appropriate range.";
-            else
-                Height = tmp_height;
-        }
-        {
-            double tmp_speed;
-            ss << argv[3];  
-            ss >> tmp_speed;
-            ss.clear();
-            if (tmp_speed < MIN_SPEED_CONST|| tmp_speed > MAX_SPEED_CONST)
-                throw "Please, select from the appropriate range.";
-            else
-                Speed = tmp_speed;
-        }
-    }
-    else
-    {
-        throw "Please, use command below.";
-    }
-}
-
-//
-void StartMenu(Client& user)
-{
-    int choice;
-    do {
-        std::cout << "1. Login\n2. SignUp\n>>> ";
-        std::cin >> choice;
-    } while (choice < 1 || choice > 2);
-
-    bool response;
-    if (choice == 1)
-    {
-        do {
-            response = user.Login();
-        } while (!response);
-    }
-    else if (choice == 2)
-    {
-        do {
-            response = user.SignUp();
-        } while (!response);
-    }
-}
-
-//
-void GameOn()
-{
-    Client user = Client();
-    
-    StartMenu(user);
-
-    Snake snake;
-    snake.InitSnake();
-    Map map;
-    map.CreateMap(Width, Height);
-
-    Game game(map, snake, user);
-    game.StartGame();
-
-    bool quit = false;
-    while (!game.IsGameEnd() && !quit)
-    {
-        quit = game.GameLoop();
-        GameSleep(1000.0 / Speed);
-    }
-
-    if (game.IsGameEnd()) 
-        game.EndGame();
-    
-    if (quit)
-        std::cout << "Program terminated!\n";
-}
-
 int main(int argc, char* argv[])
 {
     try {
-        CommandLineHandling(argc, argv);
+        SnakeGame::CommandLineHandling(argc, argv);
     } catch (const char* error) {
         std::cout << error;
         HELP_COUT
@@ -156,7 +15,8 @@ int main(int argc, char* argv[])
     }
 
     srand(time(NULL));
-    GameOn();
+    
+    SnakeGame::GameOn();
 
     getchar();
 
